d259e4ad2ca5007dbe22141507bf7c6f1e43f492
Anticrisis <anticrisisg@gmail.com>
2021-02-06 15:23:31 -1000

    Add helloworld example with no C extension.

---
+++ examples/helloworld/hello.tcl
@@ -0,0 +1,7 @@
+namespace eval hello {
+    proc hello {} {
+        puts "Hello, world."
+    }
+}
+
+package provide hello 1.0
---
+++ examples/libsodium/act.bat
@@ -0,0 +1,11 @@
+::if no {
+@tclsh "%~f0" %*
+@goto :eof
+}
+# This Windows batch file simply executes act
+# Do not change anything above this comment.
+
+source act
+
+# end tcl \
+:eof
